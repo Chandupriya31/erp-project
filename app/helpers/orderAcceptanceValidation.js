51d51d2b30a503871d2c67a7dc3e101cce785f3d
chandupriya <priyadavuluru@gmail.com>
2023-11-17 15:41:40 +0530

    order validations

--- app/helpers/orderAcceptanceValidation.js
+++ app/helpers/orderAcceptanceValidation.js
@@ -3,6 +3,9 @@ const orderValidation = {
       notEmpty: {
          errorMessage: 'quotation should be required'
       },
+      isMongoId:{
+         errorMessage: 'Quotation Id should be valid'
+      }
    }
 }
 
