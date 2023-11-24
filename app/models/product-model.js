3b4bef574861242b51bbb9f76adf2e472fba0f0f
pavan <pavanat24official@gmail.com>
2023-11-24 11:36:15 +0530

    my changes

--- app/models/product-model.js
+++ app/models/product-model.js
@@ -7,7 +7,7 @@ const productSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: 'Company'
    }],
-   perUnitCost:Number,
+   perUnitCost: Number,
    image: [{ url: String, key: String }],
    categoryId: {
       type: Schema.Types.ObjectId,
