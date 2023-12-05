97b4d53ce66eb7902a70359a29508f8b9c3b1fa3
chandupriya <priyadavuluru@gmail.com>
2023-12-05 22:19:45 +0530

    minor changes in user controller

--- app/controllers/users-ctlr.js
+++ app/controllers/users-ctlr.js
@@ -226,7 +226,7 @@ userCtlr.getCompanyDetails = async (req, res) => {
 userCtlr.updateCompany = async(req,res)=>{
     const body = req.body
     try{
-        const company = await Company.findOneAndUpdate({userId:req.userId},body,{new:true})
+        const company = await Company.findOneAndUpdate({userId:req.user.id},body,{new:true})
         res.json(company)
     }catch(e){
         res.status(500).json(e)
